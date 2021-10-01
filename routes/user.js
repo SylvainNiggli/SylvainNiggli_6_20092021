1ee35ff5d3560556d7f46b58b65d123338675382
Sylvain Niggli <sylvain.niggli@gmail.com>
2021-10-01 16:34:05 +0200

    fin du projet

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const router = express.Router();
-
 const userCtrl = require('../controllers/user');
+
+const router = express.Router();
 
 /*-- POST -- */
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
+
+/*-- GET -- */
 router.get('/signup', userCtrl.getAllUsers);
+
+/*-- DELETE -- */
 router.delete('/signup/:email', userCtrl.deleteUser);
 module.exports = router;
