0700cb01114192b0eafe73337e76c2ad2e3c1542
Sylvain Niggli <sylvain.niggli@gmail.com>
2021-10-01 17:53:43 +0200

    retirement des console.log

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,6 @@ const jsonwebtoken = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try{
         const token = req.headers.authorization.split(' ')[1];
-        console.log(token);
         const decodeToken = jsonwebtoken.verify(token, 'RANDOM_TOKEN_SECRET');
         const userId = decodeToken.userId;
         if(req.body.userId && req.body.userId !== userId) {
